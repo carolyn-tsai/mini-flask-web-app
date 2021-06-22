4ed948b83c20a64ebee89cad33ac3db4527453ec
carolyn-tsai <53666793+carolyn-tsai@users.noreply.github.com>
2021-06-21 22:17:42 -0700

    HTML and CSS added

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import os
 from flask import (
     Flask,
-    jsonify,
+    render_template,
+    request,
     abort,
-    request)
+    redirect)
+
+asset_folder = os.path.join("static", "images")
 
 app = Flask(__name__)
 
-@app.route("/test", methods=["POST"])
+app.config["image"] = asset_folder
+
+@app.route("/")
+def home_page():
+    return render_template("form.html")
+
+@app.route("/submit", methods=["POST"])
 def send_data():
-    data = request.get_json()
-    if "string_to_cut" not in data:
-        abort(400)
-    string = data["string_to_cut"]
-    # Slice(start, stop, step) - start:2 for third letter in string, stop : end of string, step:3 for every 3rd letter after
-    return_string = {"return_string" : string[2::3]}
-    return jsonify(return_string)
+    string_to_cut = request.form["string_to_cut"]   
+
+    if string_to_cut == "":
+        return_string = "Invald Input. You didn't enter anything!"
+    else:  
+        return_string = string_to_cut[2::3]
+    return render_template("submit.html", return_string=return_string)
 
 if __name__ == "__main__":
     app.run(debug=True)
